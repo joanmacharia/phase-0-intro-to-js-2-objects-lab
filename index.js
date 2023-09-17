e089bb12c961860bc47a8121018182969d46d663
Joan Macharia <joan.macharia@students.moringa>
2023-09-17 21:42:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Lucky',
+    streetAddress: '11 Nairobi'
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = { ...employee}
+//     newEmployee [key] = value
+//     return newEmployee
+// }
+     return {
+        ...employee,
+        [key]: value,
+     }
+ }
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee [key] = value
+    return employee
+}
+
+
+
+function deleteFromEmployeeByKey(employee, key ) {
+    const newEmployee = {...employee}
+    delete newEmployee [key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee [key]
+    return employee
+}
+
+
